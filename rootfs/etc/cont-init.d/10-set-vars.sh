b2a1c1fabe8a5fd95b8dc4d51952dd8bb9eb24ec
Alex <alexyao2015@gmail.com>
2021-01-03 22:50:54 -0600

    Only use options.json if exists

--- rootfs/etc/cont-init.d/10-set-vars.sh
+++ rootfs/etc/cont-init.d/10-set-vars.sh
@@ -4,22 +4,27 @@
 # This file defines environment variables based on user specified config options
 # ==============================================================================
 
-if bashio::config.has_value 'data'; then
-    export DATA_DIR=$(bashio::config 'data')
-fi
 
-if bashio::config.has_value 'lights'; then
-    export LIGHTS=$(bashio::config 'lights')
-fi
+if bashio::fs.file_exists '/data/options.json'; then
 
-if bashio::config.has_value 'token'; then
-    export HASS_TOKEN=$(bashio::config 'token')
-fi
+  if bashio::config.has_value 'data'; then
+      export DATA_DIR=$(bashio::config 'data')
+  fi
 
-if bashio::config.has_value 'url'; then
-    export HASS_URL=$(bashio::config 'url')
-fi
+  if bashio::config.has_value 'lights'; then
+      export LIGHTS=$(bashio::config 'lights')
+  fi
 
-if bashio::config.has_value 'verbose'; then
-    export VERBOSE=$(bashio::config 'verbose')
+  if bashio::config.has_value 'token'; then
+      export HASS_TOKEN=$(bashio::config 'token')
+  fi
+
+  if bashio::config.has_value 'url'; then
+      export HASS_URL=$(bashio::config 'url')
+  fi
+
+  if bashio::config.has_value 'verbose'; then
+      export VERBOSE=$(bashio::config 'verbose')
+  fi
+
 fi
